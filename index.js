14bf471ba5d887ac16f40c86998d9ad280a2f9b2
Miguel7113 <miguelndakwe11@gmail.com>
2024-07-03 18:54:03 +0300

    scope

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName(){
+   customerName = customerName.toUpperCase();
+   
+}
+console.log(customerName)
+
+     
+function setBestCustomer(){
+        bestCustomer = 'not bob'
+}
+console.log(bestCustomer)
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+
+
+function changeLeastFavoriteCustomer(){
+    const leastFavoriteCustomer = 'Jordan'
+
+   leastFavoriteCustomer = 'Pete'
+}
+console.log(leastFavoriteCustomer)
